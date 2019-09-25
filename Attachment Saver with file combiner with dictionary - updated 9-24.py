231bed41fdc6b337e1b1a1b211da25403ddc10ef
Isaac <idmather@ucdavis.edu>
2019-09-25 10:51:52 -0700

    Update Attachment Saver with file combiner with dictionary - updated 9-24.py

--- Attachment Saver with file combiner with dictionary - updated 9-24.py
+++ Attachment Saver with file combiner with dictionary - updated 9-24.py
@@ -11,31 +11,31 @@ import win32com.client as win32
 #this library is to check todays date
 import datetime
 
-#The below code helps by downloading the attachments from outlook emails that are 'Unread' (and changes the mail to Read.)
-#or from 'Today's' date.without altering the file name. Just pass the 'Subject' argument.
-#https://stackoverflow.com/questions/39656433/how-to-download-outlook-attachment-from-python-script
+##The below code helps by downloading the attachments from outlook emails that are 'Unread' (and changes the mail to Read.)
+##or from 'Today's' date.without altering the file name. Just pass the 'Subject' argument.
+##https://stackoverflow.com/questions/39656433/how-to-download-outlook-attachment-from-python-script
 
-def saveattachments(subject):
-    path = os.path.expanduser(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments')
-    today = datetime.date.today()
-    outlook = win32.Dispatch("Outlook.Application").GetNamespace("MAPI")
-    inbox = outlook.GetDefaultFolder(6) 
-    messages = inbox.Items
-    #set the title of the email you want to look for here. can we use regex to make flexible email title searches?
-    subject = 'Working with Global Analytics and Insights Team Checklist (002)'
-    for message in messages:
-        if message.Subject == subject: #and message.Unread or message.Senton.date() == today:
-            #body_content = message.body
-            attachments = message.Attachments
-            attachment = attachments.Item(1)
-            for attachment in message.Attachments:
-                attachment.SaveAsFile(os.path.join(path, str(attachment)))
-                #if message.Subject == subject and message.Unread:
-                #    message.Unread = False
-                print('Attachment Saved!')
-                break
+##def saveattachments(subject):
+##    path = os.path.expanduser(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments')
+##    today = datetime.date.today()
+##    outlook = win32.Dispatch("Outlook.Application").GetNamespace("MAPI")
+##    inbox = outlook.GetDefaultFolder(6) 
+##    messages = inbox.Items
+##    #set the title of the email you want to look for here. can we use regex to make flexible email title searches?
+##    subject = 'Working with Global Analytics and Insights Team Checklist (002)'
+##    for message in messages:
+##        if message.Subject == subject: #and message.Unread or message.Senton.date() == today:
+##            #body_content = message.body
+##            attachments = message.Attachments
+##            attachment = attachments.Item(1)
+##            for attachment in message.Attachments:
+##                attachment.SaveAsFile(os.path.join(path, str(attachment)))
+##                #if message.Subject == subject and message.Unread:
+##                #    message.Unread = False
+##                print('Attachment Saved!')
+##                break
             
-saveattachments(subject)
+##saveattachments(subject)
 
 #create a dictionary using the reference sheet. ferences the dictionary to to know which file sent from the POA team to the eloqua team to use as a resource to retrieve TaxID.
 #Then saves the combined sheet
@@ -61,7 +61,7 @@ def attachment_combiner(df):
         POA_file = df[Eloqua_file]
         os.chdir(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email List Results Sent to POA Team\Test')
         main = pd.read_excel(Eloqua_file,index_col = None)
-        os.chdir(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email Lists Sent to Eloqua Team')
+        os.chdir(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email Lists Sent to Eloqua Team\Old Files')
         secondary = pd.read_excel(POA_file,index_col = None)
         combined = pd.merge(main, secondary, sort=False, left_on=['Email Address'], right_on=['Contact: Primary Contact Email'], how = 'left')
         os.chdir(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Combined Lists')
@@ -72,21 +72,21 @@ def attachment_combiner(df):
 
 attachment_combiner(df)
 
-
-def mail_new_file():
-    #send the file in an Outlook email
-    import win32com.client
-    outlook = win32.Dispatch('outlook.application')
-    mail = outlook.CreateItem(0)
-    mail.To = 'isaama2@vsp.com'
-    mail.Subject = 'Perfect Pair Rebate List'
-    mail.Body = 'Hi Alex, \r\n\nHere is the perfect pair rebate list!\r\n\nCheers, \r\nIsaac'
-    attachment = r'\\ntsca126\PRmisc\Provider Operations and Analysis\Reporting & Analytics\Marketing\Sales\POA-1364 Perfect Pair Rebate\POA-1164 Development\python perfect pair.xlsx'
-    #attachment = directory + \ + file_name
-    print('working correctly')
-    mail.Attachments.Add(attachment)
-    mail.Send()
-    #clarify it worked
-    print('Operation successful!')
-
-mail_new_file()
+##
+##def mail_new_file():
+##    #send the file in an Outlook email
+##    import win32com.client
+##    outlook = win32.Dispatch('outlook.application')
+##    mail = outlook.CreateItem(0)
+##    mail.To = 'isaama2@vsp.com'
+##    mail.Subject = 'Perfect Pair Rebate List'
+##    mail.Body = 'Hi Alex, \r\n\nHere is the perfect pair rebate list!\r\n\nCheers, \r\nIsaac'
+##    attachment = r'\\ntsca126\PRmisc\Provider Operations and Analysis\Reporting & Analytics\Marketing\Sales\POA-1364 Perfect Pair Rebate\POA-1164 Development\python perfect pair.xlsx'
+##    #attachment = directory + \ + file_name
+##    print('working correctly')
+##    mail.Attachments.Add(attachment)
+##    mail.Send()
+##    #clarify it worked
+##    print('Operation successful!')
+##
+##mail_new_file()
