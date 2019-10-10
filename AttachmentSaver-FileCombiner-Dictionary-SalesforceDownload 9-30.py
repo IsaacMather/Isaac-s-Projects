883f937046fc8b5800a6fe5e434a08af5f0fdfd8
Isaac <idmather@ucdavis.edu>
2019-10-10 10:27:22 -0700

    Update AttachmentSaver-FileCombiner-Dictionary-SalesforceDownload 9-30.py

--- AttachmentSaver-FileCombiner-Dictionary-SalesforceDownload 9-30.py
+++ AttachmentSaver-FileCombiner-Dictionary-SalesforceDownload 9-30.py
@@ -1,3 +1,11 @@
+##              To Do List
+##              1. clean up the directories by making them dynamic
+##              2. get the new file out of the attachment combiner function
+##              3. get the file into the file mailer function
+##              4. need to retrieve the opportunity ID from salesforce\
+##              5. need to combine the opportunity id with our combined results lists before they are mailed to the OPS team
+
+
 #this library is to set the directory
 import os
 
@@ -14,66 +22,69 @@ import win32com.client as win32
 import datetime
 
 
-#####download the necessary report from salesforcefrom salesforce_reporting import Connection
-
-
-
-##The below code helps by downloading the attachments from outlook emails that are 'Unread' (and changes the mail to Read.)
-##or from 'Today's' date.without altering the file name. Just pass the 'Subject' argument.
+##The below code helps by downloading the attachments from outlook emails that are 'Unread' (and changes the mail to Read.) or from 'Today's' date.without altering the file name. Just pass the 'Subject' argument.
 ##https://stackoverflow.com/questions/39656433/how-to-download-outlook-attachment-from-python-script
-test_file_directory = 'C:\\Users\\isaama2\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Python 3.7\\Test Programs\\Test Attachments\\'
-subject = 'Working with Global Analytics and Insights Team Checklist (002)'
-
-def saveattachments(subject, test_file_directory):
-    path = os.path.expanduser(test_file_directory)
-    print(path)
-    today = datetime.date.today()
-    outlook = win32.Dispatch("Outlook.Application").GetNamespace("MAPI")
-    inbox = outlook.GetDefaultFolder(6) 
-    messages = inbox.Items
-    #set the title of the email you want to look for here. can we use regex to make flexible email title searches?
-    for message in messages:
-        if message.Subject == subject: #and message.Unread or message.Senton.date() == today:
-            #body_content = message.body
-            attachments = message.Attachments
-            attachment = attachments.Item(1)
-            for attachment in message.Attachments:
-                attachment.SaveAsFile(os.path.join(path, str(attachment)))
-                #if message.Subject == subject and message.Unread:
-                #    message.Unread = False
-                print('Attachment Saved!')
-                break
-            
-saveattachments(subject, test_file_directory)
-
-
-
-##eloqua_results = 'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email List Results Sent to POA Team\Test'
-##POA_lists = 'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email Lists Sent to Eloqua Team\Old Files'
-##combined_directory = 'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Combined Lists'
-
-
-#create a dictionary using the reference sheet. ferences the dictionary to to know which file sent from the POA team to the eloqua team to use as a resource to retrieve TaxID.
-###Then saves the combined sheet
-##os.chdir(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments')
-###get the read excel to just open the dataframe name
-##df = pd.read_excel(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\POA_Eloqua_email_and_lists.xlsx')
-##df = df.set_index('Eloqua File Name')['POA File Name'].to_dict()
-###os.chdir(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email List Results Sent to POA Team')
-###print(df)
-
-##for key, val in df.items():
-##print(key, "=>", val)
-
-
-##clean up the directories by making them dynamic
-##get the new file out of the attachment combiner function
-##get the file into the file mailer function
-##need to retrieve the opportunity ID from salesforce
+eloqua_results_directory = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\\Python 3.7\Test Programs\Test Attachments'
+##email_subject = 'Working with Global Analytics and Insights Team Checklist (002)'
 ##
-##function to find a filename and its corresponding key, then combine the file and its key
-##def attachment_combiner(df):
-##    files = os.listdir(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email List Results Sent to POA Team\Test')
+##def saveattachments(email_subject, eloqua_results_directory):
+##    path = os.path.expanduser(eloqua_results_directory)
+##    print(path)
+##    today = datetime.date.today()
+##    outlook = win32.Dispatch("Outlook.Application").GetNamespace("MAPI")
+##    inbox = outlook.GetDefaultFolder(6) 
+##    messages = inbox.Items
+##    #set the title of the email you want to look for here. can we use regex to make flexible email title searches?
+##    for message in messages:
+##        if message.Subject == email_subject: #and message.Unread or message.Senton.date() == today:
+##            #body_content = message.body
+##            attachments = message.Attachments
+##            attachment = attachments.Item(1)
+##            for attachment in message.Attachments:
+##                attachment.SaveAsFile(os.path.join(path, str(attachment)))
+##                #if message.Subject == subject and message.Unread:
+##                #    message.Unread = False
+##                print('Attachment Saved!')
+##                break
+##            
+##saveattachments(email_subject, eloqua_results_directory)
+##
+##
+##
+
+
+
+
+
+eloqua_results = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email List Results Sent to POA Team\Test'
+POA_lists = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email Lists Sent to Eloqua Team\Old Files'
+combined_directory = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Combined Lists'
+
+
+#create a dictionary using the reference sheet. references the dictionary to to know which file sent from the POA team to the eloqua team to use as a resource to retrieve TaxID. Then saves the combined sheet
+def dictionary_creater(eloqua_results_directory):
+    os.chdir(eloqua_results_directory)
+    #get the read excel to just open the dataframe name
+    df = pd.read_excel('POA_Eloqua_email_and_lists.xlsx')
+    df = df.set_index('Eloqua File Name')['POA File Name'].to_dict()
+    os.chdir(r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email List Results Sent to POA Team')
+    print(df)
+    for key, val in df.items():
+        print(key, "=>", val)
+
+dictionary_creater(eloqua_results_directory)
+
+
+
+
+
+
+
+    
+
+###function to find a filename and its corresponding key, then combine the file and its key
+##def attachment_combiner(df, eloqua_results):
+##    files = os.listdir(eloqua_results)
 ##    for Eloqua_file in files:
 ##        POA_file = df[Eloqua_file]
 ##        print(Eloqua_file)
@@ -91,8 +102,18 @@ saveattachments(subject, test_file_directory)
 ##            combined.to_excel(file_name, index=False)
 ##            print('combination complete! file saved to folder!')
 ##
-##attachment_combiner(df)
-##
+##attachment_combiner(df, eloqua_results)
+
+
+
+
+
+
+
+
+
+
+
 ##def mail_new_file():
 ##    #send the file in an Outlook email
 ##    import win32com.client
