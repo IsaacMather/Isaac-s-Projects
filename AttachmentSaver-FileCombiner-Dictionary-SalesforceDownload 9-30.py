5cc951a90da06109fb1128fa7a35948f671bc08f
Isaac <idmather@ucdavis.edu>
2019-10-28 18:08:05 -0700

    Update AttachmentSaver-FileCombiner-Dictionary-SalesforceDownload 9-30.py

--- AttachmentSaver-FileCombiner-Dictionary-SalesforceDownload 9-30.py
+++ AttachmentSaver-FileCombiner-Dictionary-SalesforceDownload 9-30.py
@@ -1,14 +1,7 @@
-##              To Do List
-##              1. clean up the directories by making them dynamic
-##              2. get the new file out of the attachment combiner function
-##              3. get the file into the file mailer function
-##              4. need to retrieve the opportunity ID from salesforce\
-##              5. need to combine the opportunity id with our combined results lists before they are mailed to the OPS team
-
 ############to make this work
 #1. Set the email subject on line 37
 #2. Set the sheet names in the POA lists to their equivelant sheet in the Eloqua results
-#3. Update the POA_Eloqua_email_and_lists.xlsx dataframe with the corfrelating POA lists and Eloqua Results names
+#3. Update the POA_Eloqua_email_and_lists.xlsx dataframe with the correlating POA lists and Eloqua Results names
 #4. Set the eloqua_results_file_locations for the correct month
 #5. Set the POA_lists_file_location for the correct month
 
@@ -26,11 +19,15 @@ import win32com.client as win32
 #this library is to check todays date
 import datetime
 
-
-##The below code helps by downloading the attachments from outlook emails that are 'Unread' (and changes the mail to Read.) or from 'Today's' date.without altering the file name. Just pass the 'Subject' argument.
-##https://stackoverflow.com/questions/39656433/how-to-download-outlook-attachment-from-python-script
-eloqua_results_file_locations = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email List Results Sent to POA Team\September'
 email_subject = 'FW: September'
+POA_Eloqua_Team_Dataframe_Location = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments'
+eloqua_results_file_locations = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email List Results Sent to POA Team\September'
+combined_results_file_location = r'C:\Users\isaama2\Desktop\Eloqua Data Combiner Files\Test File Combiner Folder'
+POA_lists_file_location = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email Lists Sent to Eloqua Team\September Files'
+opportunity_ID_file_location = r'C:\Users\isaama2\Desktop\Eloqua Data Combiner Files\Opportunity ID Reference Folder\Opportunity ID Reference File.xlsx' 
+new_combined_file_with_opportunity_ID_directory = r'C:\Users\isaama2\Desktop\Eloqua Data Combiner Files\Test File Combiner Folder\Test Files With Opportunity ID'
+
+#This function goes into Outlook, and pulls out files from specific email titles we name. This is how we get the data to begin with. Credit goes to https://stackoverflow.com/questions/39656433/how-to-download-outlook-attachment-from-python-script
 def saveattachments(email_subject, eloqua_results_file_locations):
     path = eloqua_results_file_locations
     #print(path)
@@ -46,15 +43,12 @@ def saveattachments(email_subject, eloqua_results_file_locations):
             attachment = attachments.Item(1)
             for attachment in message.Attachments:
                 attachment.SaveAsFile(os.path.join(path, str(attachment)))
-                #if message.Subject == subject and message.Unread:
-                #    message.Unread = False
                 print('Attachment Saved!')
 
-saveattachments(email_subject, eloqua_results_file_locations)
+##saveattachments(email_subject, eloqua_results_file_locations)
 
 
-###~##create a dictionary using the reference sheet. references the dictionary to to know which file sent from the POA team to the eloqua team to use as a resource to retrieve TaxID. Then saves the combined sheet
-POA_Eloqua_Team_Dataframe_Location = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments'
+##This function creates a 'dictionary' that we use to combine the email lists POA sends, with the results we get back from the Eloqua team. We can ask the dictionary, "what's the corresponding file for this particular name" and it will tell us. 
 def dictionary_creater(POA_Eloqua_Team_Dataframe_Location):
     os.chdir(POA_Eloqua_Team_Dataframe_Location)
     #get the read excel to just open the dataframe name
@@ -66,14 +60,7 @@ def dictionary_creater(POA_Eloqua_Team_Dataframe_Location):
 eloqua_results_dictionary = dictionary_creater(POA_Eloqua_Team_Dataframe_Location)
 
 
-###~~~this code checks if we successfully created the dictionary to correlate eloqua results files with POA lists
-##for key, val in eloqua_results_dictionary.items():
-##    print(key, "=>", val)
-    
-
 ###function to find a filename and its corresponding key, then combine the file and its key
-combined_results_file_location = r'C:\Users\isaama2\Desktop\Eloqua Data Combiner Files\Test File Combiner Folder'
-POA_lists_file_location = r'C:\Users\isaama2\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Python 3.7\Test Programs\Test Attachments\Email Lists Sent to Eloqua Team\September Files'
 def attachment_combiner(eloqua_results_dictionary, eloqua_results_file_locations, POA_lists_file_location, combined_results_file_location):
     files = os.listdir(POA_lists_file_location)
     for POA_file in files:
@@ -89,8 +76,7 @@ def attachment_combiner(eloqua_results_dictionary, eloqua_results_file_locations
             secondary = pd.read_excel(POA_file, sheet_name = POA_sheet, index_col = None)
             os.chdir(eloqua_results_file_locations)
             main = pd.read_excel(Eloqua_file, sheet_name = POA_sheet, index_col = None)
-            combined = pd.merge(main, secondary[['Contact: Primary Contact Email','Tax ID']], sort=False, left_on=['Email Address'], right_on=['Contact: Primary Contact Email'], how = 'left')  #get this cleanup drop working .drop('First Name','Last Name','SFDC Contact ID','Email Send Date')
- #make sure you are keeping just TaxID
+            combined = pd.merge(main, secondary[['Contact: Primary Contact Email','Tax ID']], sort=False, left_on=['Email Address'], right_on=['Contact: Primary Contact Email'], how = 'left').drop(columns=['First Name','Last Name','SFDC Contact ID','Email Send Date','Contact: Primary Contact Email','Total Possible Forwards','Total Hard Bouncebacks'])
             os.chdir(combined_results_file_location)
             file_name = POA_file + ' + ' + POA_sheet + ' done by Python.xlsx'
             combined.to_excel(file_name, index=False)
@@ -99,16 +85,14 @@ def attachment_combiner(eloqua_results_dictionary, eloqua_results_file_locations
 attachment_combiner(eloqua_results_dictionary, eloqua_results_file_locations, POA_lists_file_location, combined_results_file_location)
 
 
-#function to combine the results sheet, with the opportunity ID list, so they can be uploaded to salesforce
-opportunity_ID_file_location = r'C:\Users\isaama2\Desktop\Eloqua Data Combiner Files\Opportunity ID Reference Folder\Opportunity ID Reference File.xlsx' 
-new_combined_file_with_opportunity_ID_directory = r'C:\Users\isaama2\Desktop\Eloqua Data Combiner Files\Test File Combiner Folder\Test Files With Opportunity ID'
+#This function takes the product of the previous function, and combined it with a reference file for opportunity ID's. The product of this function is to have email results data combined with TaxID and Opportunity ID. Now the Eloqua results data is ready for uploading. 
 def opportunity_ID_combiner(opportunity_ID_file_location, combined_results_file_location, new_combined_file_with_opportunity_ID_directory):
     files = os.listdir(combined_results_file_location)
     for combined_file in files:
                 os.chdir(combined_results_file_location)
                 combined_results_file = pd.read_excel(combined_file, index_col = None)
                 opportunity_ID_file = pd.read_excel(opportunity_ID_file_location, index_col = None)  #it is set up for .xlsx
-                combined = pd.merge(combined_results_file, opportunity_ID_file[['Opportunity ID','Tax ID']], on = 'Tax ID', how = 'left') #set it to only keep the opportunity ID
+                combined = pd.merge(combined_results_file, opportunity_ID_file[['Opportunity ID','Tax ID']], on = 'Tax ID', how = 'left') 
                 file_name = combined_file
                 os.chdir(new_combined_file_with_opportunity_ID_directory)
                 combined.to_excel(file_name, index = False)
@@ -117,7 +101,7 @@ def opportunity_ID_combiner(opportunity_ID_file_location, combined_results_file_
 opportunity_ID_combiner(opportunity_ID_file_location, combined_results_file_location, new_combined_file_with_opportunity_ID_directory)
 
 
-#function to send each file that has been combined with Opportunity ID and in an Outlook email
+#This function sends each complete file to a person we choose, with the email subject and content we choose
 def mail_the_files_to_ops(new_combined_file_with_opportunity_ID_directory):
         outlook = win32.Dispatch('outlook.application')
         files = os.listdir(new_combined_file_with_opportunity_ID_directory)
@@ -132,4 +116,4 @@ def mail_the_files_to_ops(new_combined_file_with_opportunity_ID_directory):
             #clarify it worked
         print('Operation successful!')
 
-mail_the_files_to_ops(new_combined_file_with_opportunity_ID_directory)
+#mail_the_files_to_ops(new_combined_file_with_opportunity_ID_directory)
