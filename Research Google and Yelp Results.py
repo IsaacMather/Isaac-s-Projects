532925db04d154dad69a067f243e4afa561dce9f
Isaac <idmather@ucdavis.edu>
2019-10-31 16:13:25 -0700

    Update Research Google and Yelp Results.py

--- Research Google and Yelp Results.py
+++ Research Google and Yelp Results.py
@@ -8,7 +8,7 @@
 ##Take a practice I know exists, use it as a test, have the program rip through the spreadsheet of addresses and return search results. 
 ##
 ##Create a flow chart, and reconnect with Justin.
-
+##search google maps for the search result, and see if it is open or closed
 
 #1. iterate through the sheets, combining name and address into a text field that we search for. Return the top 5 search results for this thing. 
 
@@ -19,6 +19,8 @@
 ##https://pypi.org/project/google/
 ##https://cmdlinetips.com/2018/12/how-to-loop-through-pandas-rows-or-how-to-iterate-over-pandas-rows/\
 
+#flowchart https://code2flow.com/3sskBv
+
 from googlesearch import search
 import os
 import pandas as pd
@@ -28,8 +30,8 @@ file_location_of_list_of_practices = '' #need to add this practice file
 def search_for_web_results(file_location_of_list_of_practices):
     practices = pd.ExcelFile(file_location_of_list_of_practices)
     for index, row in practices.iterrows(): #don't use this, use https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas/55557758#55557758
-        for url in search('"Breaking Code" WordPress blog', stop=20):
-        print(url)
+        for url in search(row[practice_name]+row[practice+address], stop=1): #google maps not google search
+            #here we set the open/closed variable result, to a row 
 
 
 
