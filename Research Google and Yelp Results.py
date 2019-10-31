9469acbe14eeab2c279133b8943c60cd43c373f4
Isaac <idmather@ucdavis.edu>
2019-10-30 17:26:57 -0700

    Update Research Google and Yelp Results.py

--- Research Google and Yelp Results.py
+++ Research Google and Yelp Results.py
@@ -9,9 +9,29 @@
 ##
 ##Create a flow chart, and reconnect with Justin.
 
+
+#1. iterate through the sheets, combining name and address into a text field that we search for. Return the top 5 search results for this thing. 
+
+
+
+
 ##https://python-googlesearch.readthedocs.io/en/latest/
 ##https://pypi.org/project/google/
+##https://cmdlinetips.com/2018/12/how-to-loop-through-pandas-rows-or-how-to-iterate-over-pandas-rows/\
 
 from googlesearch import search
-for url in search('"Breaking Code" WordPress blog', stop=20):
-    print(url)
+import os
+import pandas as pd
+import numpy as np
+
+file_location_of_list_of_practices = '' #need to add this practice file
+def search_for_web_results(file_location_of_list_of_practices):
+    practices = pd.ExcelFile(file_location_of_list_of_practices)
+    for index, row in practices.iterrows(): #don't use this, use https://stackoverflow.com/questions/16476924/how-to-iterate-over-rows-in-a-dataframe-in-pandas/55557758#55557758
+        for url in search('"Breaking Code" WordPress blog', stop=20):
+        print(url)
+
+
+
+#search_for_web_results(file_location_of_list_of_practices)
+
