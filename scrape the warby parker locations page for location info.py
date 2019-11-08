e2bf10695a99102dfb82c318ff66af9898787f26
Isaac <idmather@ucdavis.edu>
2019-11-08 14:06:59 -0800

    Create scrape the warby parker locations page for location info.py

---
+++ scrape the warby parker locations page for location info.py
@@ -0,0 +1,7 @@
+#scrape the warby parker web page for locationbs, format it so it is excel friendly,
+
+#street, city state, zip, etc,
+
+#have a constantly updated list for the warby parker locations,
+
+#also any of the other providers
